23b2f8d3fe818ed4057805a99ff8db39ce23961c
skapxd <hbiaser132@gmail.com>
2021-09-02 22:45:27 -0500

    create scaffold folders

---
+++ src/screens/DetailsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const nameDetailsScreen = 'nameDetailsScreen'
+
+export const DetailsScreen = () => {
+    return (
+        <View>
+            <Text>Details</Text>
+        </View>
+    )
+}
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const nameHomeScreen = 'nameHomeScreen'
+
+export const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Hola</Text>
+        </View>
+    )
+}
